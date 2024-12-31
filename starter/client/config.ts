0cbf558b1aa5e81d66e88aa8283c1e6d5b659f2b
hungptit2809 <hungptit2809@gmail.com>
2024-12-31 23:12:57 +0700

    Add files via upload

---
+++ starter/client/config.ts
@@ -0,0 +1,8 @@
+const apiId = '6270qzgf7b'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-3ca3p63exwsb7sny.us.auth0.com',    // Domain from Auth0
+  clientId: 'iePi5GCMCt2Ws5e88qzqSMaKJL8dKUFd',  // Client id from an Auth0 application
+  callbackUrl: 'http://localhost:3000/callback'
+}
